4f7c64a708ca456aae60b8edb4e48c1efd1e00a0
ShadyPinesMa <69599864+ShadyPinesMa@users.noreply.github.com>
2020-10-03 16:37:44 -0400

    Different weather stats
    Set up javascript in a function to display currrent data for city name, weather description, humidity, and wind

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 function showTemperature(response) {
   console.log(response.data);
   let temperatureNumber = document.querySelector("#temperature");
+  let cityName = document.querySelector("#city");
+  let weatherDescription = document.querySelector("#current-status");
+  let humidity = document.querySelector("#humidity");
+  let windSpeed = document.querySelector("#wind");
   temperatureNumber.innerHTML = Math.round(response.data.main.temp);
+  cityName.innerHTML = response.data.name;
+  weatherDescription.innerHTML = response.data.weather[0].description;
+  humidity.innerHTML = response.data.main.humidity;
+  windSpeed.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "5a533b6a6d16b85bbee4c6b85f37d1be";
