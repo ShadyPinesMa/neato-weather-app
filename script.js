27b8bb05c7b994e535330a6519207543f27ee598
ShadyPinesMa <69599864+ShadyPinesMa@users.noreply.github.com>
2020-10-03 17:28:27 -0400

    Time/day
    Added function for current day/time

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+function currentDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let dayNight = `AM`;
+  if (hours > 12) {
+    dayNight = `PM`;
+    hours = hours - 12;
+  }
+  else {dayNight = `AM`;}
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes} ${dayNight}`;
+}
+
+
 function showTemperature(response) {
   console.log(response.data);
   let temperatureNumber = document.querySelector("#temperature");
@@ -5,11 +27,13 @@ function showTemperature(response) {
   let weatherDescription = document.querySelector("#current-status");
   let humidity = document.querySelector("#humidity");
   let windSpeed = document.querySelector("#wind");
+  let date = document.querySelector("#current-day-time")
   temperatureNumber.innerHTML = Math.round(response.data.main.temp);
   cityName.innerHTML = response.data.name;
   weatherDescription.innerHTML = response.data.weather[0].description;
   humidity.innerHTML = response.data.main.humidity;
   windSpeed.innerHTML = Math.round(response.data.wind.speed);
+  date.innerHTML = currentDate(response.data.dt * 1000);
 }
 
 let apiKey = "5a533b6a6d16b85bbee4c6b85f37d1be";
